d196c3161b36ca11f7a28e2720ad7461885db48a
Caique Caleiro <caiquecaleiro@gmail.com>
2016-04-29 21:53:30 +0530

    Add getWeatherData and buildForecasts functions

--- src/app/weather/weather.controller.js
+++ src/app/weather/weather.controller.js
@@ -9,5 +9,24 @@
 
   function WeatherController(weatherService) {
     var vm = this;
+    vm.city = 'Blumenau';
+    vm.state = 'SC';
+    vm.getWeatherData = getWeatherData;
+    vm.recomendation
+    vm.forecasts = [];
+    vm.tempMin = 0;
+    vm.tempMax = 0;
+    vm.tempDay = 0;
+
+    function getWeatherData(city, state) {
+      weatherService.getWeatherData(city, state)
+        .success(function(data) {
+          buildForecasts(data);
+        });
+    }
+
+    function buildForecasts(data) {
+      weatherService.buildForecasts(vm.forecasts, data);
+    }
   }
 })();
--- src/app/weather/weather.service.js
+++ src/app/weather/weather.service.js
@@ -8,8 +8,67 @@
   weatherService.$inject = ['$http'];
 
   function weatherService($http) {
-    var service = {};
+    var service = {
+      getWeatherData: getWeatherData,
+      buildForecasts: buildForecasts
+    };
 
     return service;
+
+    /**
+    * Returns the forecast data of today and the next 6 days.
+    * @param {string} city - The city name.
+    * @returns {json} The forecast data as json
+    */
+    function getWeatherData(city) {
+      var apiKey = '&APPID=0e6bfb61dab954b920834902e14fa852';
+      var units = '&units=metric';
+      var mode = '&mode=json';
+      var callback = '&callback=JSON_CALLBACK';
+      var days = '&cnt=7';
+      var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + ',br'
+        + mode + units + days + apiKey + callback;
+
+      return $http.jsonp(url);
+    }
+
+    /**
+     * Checks all the forecasts data and creates new Forecast object that is added
+     * to the forecasts object.
+     * @param {object} forecasts - The forecasts object.
+     * @param {object} data - The forecast data.
+     */
+    function buildForecasts(forecasts, data) {
+      for (var i = 0; i < data.list.length; i++) {
+        var forecastData = data.list[i].temp;
+        forecasts.push(new Forecast(forecastData.min,
+          forecastData.max,
+          forecastData.day
+        ));
+      }
+      // add method for weekends temperature??
+    }
+
+    /**
+     * Converts timestamp to date.
+     * @param {number} date - The Unix timestamp value.
+     * @returns {Date}
+     */
+    function timestampToDate(date) {
+      return new Date(date * 1000);
+    }
+
+    /**
+     * Represents a forecast
+     * @constructor
+     * @param {number} tempMin - The minimum temperature.
+     * @param {number} tempMax - The maximum temperature.
+     * @param {number} tempDay - The current temperature.
+     */
+    function Forecast(tempMin, tempMax, tempDay) {
+      this.tempMin = tempMin;
+      this.tempMax = tempMax;
+      this.tempDay = tempDay;
+    }
   }
 })();
