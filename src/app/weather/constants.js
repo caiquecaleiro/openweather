a32bdc3e39dd8f0ee224119b68ca36edc1ee92a1
Caique Caleiro <caiquecaleiro@gmail.com>
2016-04-30 22:43:23 +0530

    Add module constants

---
+++ src/app/weather/constants.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.weather')
+        .constant('initialData', {
+          BLUMENAU: 0,
+          SANTA_CATARINA: 22
+        });
+})();
